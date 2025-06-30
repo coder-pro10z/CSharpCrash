75457956b9085f26dba09bce93a3c191f4eb7c3f
coder-pro10z <2pkashyap2001@gmail.com>
2025-06-30 23:47:32 +0530

    Add multiplication table generator and project setup

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,22 @@
+﻿//MultiplicationTable
+
+class MultiplicationTable
+{
+    public static void Main()
+    {
+        Console.WriteLine("Multiplication Table");
+        int tablesCount = 10;
+        int count = 1;
+        while (count <= tablesCount)
+        {
+            int multiplier = 1;
+            while (multiplier <= 10)
+            {
+                Console.Write("{0}\t|", count * multiplier);
+                multiplier++;
+            }
+            Console.WriteLine();
+            count++;
+        }
+    }
+}
