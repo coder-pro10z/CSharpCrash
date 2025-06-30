1accd79e2e37065bf91188c7208bd778537e6703
coder-pro10z <2pkashyap2001@gmail.com>
2025-07-01 02:36:22 +0530

    Add CreditCardMasker project to mask credit card numbers
    Introduced a new `CreditCardMasker` project to the solution (`CSharpCrash.sln`).
    
    Implemented masking functionality in `Program.cs` to replace all but the
    last four digits of a credit card number with 'X', preserving dashes and
    whitespace. Updated the solution file to include the new project.

---
+++ CreditCardMasker/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+
+class CreditCardMasker
+{
+    public static void Main()
+    {
+        string card_num;
+        //bool isValid;
+
+        do
+        {
+            Console.WriteLine("Enter a Credit Card Number:");
+            card_num = Console.ReadLine();
+        }while (String.IsNullOrEmpty(card_num));
+
+        string maskedNum=String.Empty;
+
+        //Console.WriteLine(card_num);
+
+        for (int i = 0; i < card_num.Length; i++)
+        {
+            if (card_num[i] == '-' || Char.IsWhiteSpace(card_num[i]) || i >= card_num.Length - 4)
+                maskedNum += card_num[i];
+            else
+                maskedNum += 'X';
+        }
+    Console.WriteLine(maskedNum);
+    }
+}
