c4aa45d30644e7a56874ab4908eed2c429e068cb
coder-pro10z <2pkashyap2001@gmail.com>
2025-07-01 23:53:18 +0530

    Add new project with user-defined data types demo
    Introduced a new project `UserDefinedDataTypePath` to the solution.
    Added `ConfigPath` and `Config` classes in `Program.cs` to demonstrate
    user-defined data types with default file path values.(initialized values)

---
+++ UserDefinedDataTypePath/Program.cs
@@ -0,0 +1,29 @@
+﻿public class ConfigPath
+{
+     public string Path1 { get; set; }
+     public string Path2 { get; set; }
+}
+
+public class Config
+{ public ConfigPath Paths { get; set; }
+    public Config()
+    {
+        // Initialize Paths with default values
+        Paths = new ConfigPath
+        {
+            Path1 = @"C:\Input\input.txt",
+            Path2 = @"C:\Output\output.csv"
+        };
+    }
+}
+    public class UserDefinedDataTypeIntro {
+        public static void Main() {
+            Console.WriteLine("Hello World!");
+            
+            Config config = new Config();
+
+            Console.WriteLine(config.Paths.Path1);
+            
+        }
+    }
+
