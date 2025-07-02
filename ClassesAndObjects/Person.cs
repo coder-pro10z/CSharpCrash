f2889280d53c1ff8657579a3a328ac406ae9c87e
coder-pro10z <2pkashyap2001@gmail.com>
2025-07-02 23:36:26 +0530

    Add new project 'ClassesAndObjects' with Person class
    Introduced a new project 'ClassesAndObjects' targeting .NET 8.0.
    Added a `Person` class with private fields, constructors, and
    getter/setter methods for managing first and second names.
    Created a `Program.cs` file to demonstrate usage of the `Person`
    class. Updated solution file with build configurations for the
    new project.

---
+++ ClassesAndObjects/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    public class Person
+    {
+        private string? firstName; //string? declaring a nullable string
+        private string? secondName;
+
+        //default constructor - no arguments
+        public Person()
+        {
+
+        }
+
+        //parameterized constructor - 2 params
+        public Person(string fn, string ln)
+        {
+            firstName = fn;
+            secondName = ln;
+        }
+
+        public string GetFirstName()
+        { 
+            return firstName;
+        }
+
+        public string GetSecondName()
+        {
+            return secondName;
+        }
+
+        public void SetFirstName(string _firstname)
+        {
+            firstName = _firstname;
+        }
+
+        public void SetSecondName(string _secondname)
+        {
+            secondName = _secondname;
+        }
+
+
+
+
+
+    }
+}
---
+++ ClassesAndObjects/Program.cs
@@ -0,0 +1,12 @@
+﻿using ClassesAndObjects;
+
+Person FirstPerson=new Person(); //Initializing a object from the Person Class - FirstPerson
+Person SecondPerson=new Person();
+
+//Setting the FirstName of the Instance FirstPerson as "Anam"
+FirstPerson.SetFirstName("Anam");
+FirstPerson.SetSecondName("Ansari");
+
+
+//Getting the FirstName of the Instance FirstPerson as "Anam" and Printing it 
+Console.WriteLine($"The First Name of the FirstPerson is {FirstPerson.GetFirstName()} {FirstPerson.GetSecondName()}.");
