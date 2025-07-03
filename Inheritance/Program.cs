5168461eeecd0a3e8d6df8ed5f8268925d6ef333
coder-pro10z <2pkashyap2001@gmail.com>
2025-07-03 23:50:48 +0530

    Add Inheritance project with Person and Student classes
    Added a new `Inheritance` project to the solution (`CSharpCrash.sln`) targeting `.NET 8.0`.
    Created `Person` and `Student` classes to demonstrate single inheritance in C#.
    Added a `Program.cs` file showcasing the usage of the `Student` class.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,10 @@
+﻿using Inheritance;
+using System.Globalization;
+
+Student student1 = new Student();
+student1.gpa = 3;
+student1.Name = "Praveen";
+student1.Id = 1;
+//student1.gpa = 3;
+
+Console.WriteLine($"{student1.Name}'s ID is {student1.Id} and Gpa is :{student1.gpa}");
---
+++ Inheritance/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Student : Person  //CSharp has only single Inheritence, but you can inherit multiple interfaces
+    {
+        public int gpa { get; set; }
+        public int Id { get; set; }
+
+
+    }
+}
